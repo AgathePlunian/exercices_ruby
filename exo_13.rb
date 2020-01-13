33e2b26d6a178edf7793b966ae51e497fb7d2ea7
axaidan <axaidan@hotmail.com>
2020-01-13 18:32:35 +0100

    rendu exo_13

---
+++ exo_13.rb
@@ -0,0 +1,9 @@
+puts "Veuillez entrer votre annee de naissance :"
+print "> "
+
+annee_naissance = gets.to_i
+
+while annee_naissance <= 2020 do
+	puts annee_naissance
+	annee_naissance = annee_naissance + 1
+end
