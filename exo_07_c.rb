fd9adbbbf9c374759589ee01a5315c257c6b7787
axaidan <axaidan@hotmail.com>
2020-01-13 17:13:54 +0100

    rendu exo_07_a,b,c

---
+++ exo_07_c.rb
@@ -0,0 +1,5 @@
+user_name = gets.chomp
+puts user_name
+#le premier fichier donne la consigne, laisse l'user entrer son blase, puis affiche le blase
+#le second donne la consigne, le print indique a l'user qu'il peut entrer une donnee, puis puts qffiche la donnee entree
+#le troisieme ne donne aucune consigne, donne a l'utilisateur la possibilite d'entrer une donnee sans info, puis puts affiche la donnee entree
