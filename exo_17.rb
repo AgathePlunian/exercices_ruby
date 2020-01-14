73eab3f35d10cdbe0d48d8eb808aa4d60c2668cc
AgathePlunian <agathe.plunian@gmail.com>
2020-01-14 15:03:59 +0100

    exo_17 exo_18

---
+++ exo_17.rb
@@ -0,0 +1,15 @@
+puts "Quel age as tu ?"
+x = gets.to_i
+
+compteur = 0 
+
+x.times do 
+    x = x - 1
+     compteur = compteur + 1
+     puts " Il y a #{x} ans, tu avais #{compteur} ans."
+
+if x == compteur
+
+    puts " Il y a #{x} ans, tu avais la moitié de l'âge que tu as aujourd'hui."
+end
+end
---
+++ exo_18.rb
@@ -0,0 +1,17 @@
+my_array= []
+
+
+compteur = 0
+
+50.times do
+    compteur = compteur +01
+puts "jean.dupont.#{compteur}email.fr"
+
+my_array << "jean.dupont.#{compteur}email.fr"
+
+end
+
+print my_array
+
+
+
